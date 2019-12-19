40aee8ef7cd349652b4410e42862792d31166d81
Maydaytyh <Maydaytyh@gmail.com>
2019-12-19 09:27:37 +0800

    尝试将输出都存入一个文件再输出，但时间还是十多秒。。

--- suduku.cpp
+++ suduku.cpp
@@ -5,8 +5,10 @@ using namespace std;
 /****                                   变量定义区                           ****/
 const int FirstNum = 5;//(2+2)%9+1=5
 int sudoku[10][10];
+int nn;
 int num[9] = { 1,2,3,4,6,7,8,9 };
 int offset[8] = { 3, 6, 1, 4, 7, 2, 5, 8 };
+char sudodo[1000005][10][10];
 int get(char* num)
 {
     int len = strlen(num);
@@ -15,107 +17,45 @@ int get(char* num)
         ans = ans * 10 + num[i] - '0';
     return ans;
 }
-struct IO {
-#define MAXSIZE (1 << 20)
-#define isdigit(x) (x >= '0' && x <= '9')
-    char buf[MAXSIZE], * p1, * p2;
-    char pbuf[MAXSIZE], * pp;
-#if DEBUG
-#else
-    IO() : p1(buf), p2(buf), pp(pbuf) {}
-    ~IO() { fwrite(pbuf, 1, pp - pbuf, stdout); }
-#endif
-    inline char gc() {
-#if DEBUG  //调试，可显示字符
-        return getchar();
-#endif
-        if (p1 == p2) p2 = (p1 = buf) + fread(buf, 1, MAXSIZE, stdin);
-        return p1 == p2 ? ' ' : *p1++;
-    }
-    inline bool blank(char ch) {
-        return ch == ' ' || ch == '\n' || ch == '\r' || ch == '\t';
-    }
-    template <class T>
-    inline void read(T& x) {
-        register double tmp = 1;
-        register bool sign = 0;
-        x = 0;
-        register char ch = gc();
-        for (; !isdigit(ch); ch = gc())
-            if (ch == '-') sign = 1;
-        for (; isdigit(ch); ch = gc()) x = x * 10 + (ch - '0');
-        if (ch == '.')
-            for (ch = gc(); isdigit(ch); ch = gc())
-                tmp /= 10.0, x += tmp * (ch - '0');
-        if (sign) x = -x;
-    }
-    inline void read(char* s) {
-        register char ch = gc();
-        for (; blank(ch); ch = gc())
-            ;
-        for (; !blank(ch); ch = gc()) *s++ = ch;
-        *s = 0;
-    }
-    inline void read(char& c) {
-        for (c = gc(); blank(c); c = gc())
-            ;
-    }
-    inline void push(const char& c) {
-#if DEBUG  //调试，可显示字符
-        putchar(c);
-#else
-        if (pp - pbuf == MAXSIZE) fwrite(pbuf, 1, MAXSIZE, stdout), pp = pbuf;
-        *pp++ = c;
-#endif
-    }
-    template <class T>
-    inline void write(T x) {
-        if (x < 0) x = -x, push('-');  // 负数输出
-        static T sta[35];
-        T top = 0;
-        do {
-            sta[top++] = x % 10, x /= 10;
-        } while (x);
-        while (top) push(sta[--top] + '0');
-    }
-    template <class T>
-    inline void write(T x, char lastChar) {
-        write(x), push(lastChar);
-    }
-} io;
 void print(int ans[10][10])
 {
-    char sudo[10][10];
+    /* char sudo[10][10];
+     for (int i = 0; i < 9; ++i)
+     {
+         for (int j = 0; j < 9; ++j)
+         {
+             sudo[i][j] = ans[i][j] + '0';
+         }
+         sudo[i][9] = '\0';
+     }
+     for (int i = 0; i < 9; ++i) printf("%s\n", sudo[i]);*/
+     /*for (int i = 0; i < 9; ++i)
+     {
+         for (int j = 0; j < 8; ++j)
+         {
+             putchar(sudo[i][j]);
+             putchar(' ');
+         }
+         putchar(sudo[i][8]);
+         printf("\n");
+     }*/
+     /* for (int i = 1; i <= 9; ++i)
+      {
+          for (int j = 1; j <= 8; ++j)
+          {
+              io.write(ans[i][j]);
+              putchar(' ');
+          }
+          io.write(ans[i][9]);
+          putchar(' ');
+      }*/
+      /* puts("");*/
     for (int i = 0; i < 9; ++i)
     {
         for (int j = 0; j < 9; ++j)
-        {
-            sudo[i][j] = ans[i][j] + '0';
-        }
-        sudo[i][9] = '\0';
+            sudodo[nn][i][j] = ans[i][j] + '0';
+        sudodo[nn][i][9] = '\0';
     }
-    //for (int i = 0; i < 9; ++i) printf("%s\n", sudo[i]);
-    /*for (int i = 0; i < 9; ++i)
-    {
-        for (int j = 0; j < 8; ++j)
-        {
-            putchar(sudo[i][j]);
-            putchar(' ');
-        }
-        putchar(sudo[i][8]);
-        printf("\n");
-    }*/
-    for (int i = 1; i <= 9; ++i)
-    {
-        for (int j = 1; j <= 8; ++j)
-        {
-            io.write(ans[i][j]);
-            putchar(' ');
-        }
-        io.write(ans[i][9]);
-        putchar(' ');
-    }
-    puts("");
 }
 
 int main(int argc, char** argv)
@@ -131,7 +71,7 @@ int main(int argc, char** argv)
     int n = get(argv[2]);
     sudoku[1][1] = 5;
 
-    int nn = 0;
+    nn = 0;
     do
     {
         //构造第一行
@@ -147,8 +87,8 @@ int main(int argc, char** argv)
                 sudoku[i][shifted] = sudoku[1][j];
             }
         }
-        print(sudoku);
         nn++;
+        print(sudoku);
         if (nn > n) goto out;
         int sudoku_1[10][10];
         //接着构造
@@ -170,14 +110,22 @@ int main(int argc, char** argv)
                     for (int j = 1; j <= 9; ++j)
                         sudoku_1[i][j] = sudoku[num_3[i - 7] + 7][j];
                 }
-                print(sudoku_1);
                 nn++;
+                print(sudoku_1);
+                //nn++;
                 if (nn > n) goto out;
             } while (next_permutation(num_3, num_3 + 3));
         } while (next_permutation(num_2, num_2 + 3));
     } while (next_permutation(num, num + 8));
 out:
-
+    for (int i = 1; i <= nn; ++i)
+    {
+        for (int j = 0; j < 9; ++j)
+        {
+            printf("%s\n", sudodo[i][j]);
+        }
+        puts("");
+    }
     // fclose(stdout);
     freopen("CON", "w", stdout);
     puts("打印完成！");
